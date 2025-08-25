9c517c427fb0779f5cc9c37cec7bbfeed73221fa
26marat <marat@zhukau.com>
2025-08-25 19:52:04 -0400

    Initial commit

---
+++ src/main/java/com/maratzhukau/frauddetection/TransactionFraudDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.maratzhukau.frauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionFraudDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionFraudDetectionApplication.class, args);
+	}
+
+}
